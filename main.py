1c14e3140d0e68b2e0b6b945ff2b601e7df5ec53
app-kaisha <app.kaihatsusha@gmail.com>
2024-02-13 12:30:31 +0000

    player input requests created

--- main.py
+++ main.py
@@ -14,27 +14,42 @@ def display_board(board):
     print("---------")
     print(board[6] + " | " + board[7] + " | " + board[8])
 
+def make_player_move():
+    pass
+
 
 if __name__ == "__main__":
-    print("TicTacToe")
+    print("TicTacToe\n")
+
+    players = {
+        1: "X",
+        2: "O"
+    }
 
     game_board = GameBoard()
 
     play_game = True
-
-    # TODO-1 Inform Players to decide who is X to go first
-    # TODO-2 Ask player to enter grid position for symbol
-    # TODO-3 Check player position is valid
-    # TODO-4 Update game board with position
-    # TODO-5 Check if win condition exists
-    # TODO-6 Change Player
-    # TODO-7 Inform Winner
-    # TODO-8 Ask to play again
-    # TODO-9 Play Again or End game
+    current_player = 1
 
     game_board.board[0] = 'X'
     game_board.board[4] = 'X'
     game_board.board[8] = 'X'
+
+    # TODO-1 Inform Players to decide who is X to go first
+    print(f'Player {current_player} please select a grid position to place your {players[current_player]}')
+    # TODO-2 Ask player to enter grid position for symbol
+    player_move = input("Position: ")
+    # TODO-3 Check player position is valid
+    if game_board.board[int(player_move)] != " ":
+        print("Sorry that position is already taken! Select another!")
+    else:
+        pass
+        # TODO-4 Update game board with position
+        # TODO-5 Check if win condition exists
+        # TODO-6 Change Player
+        # TODO-7 Inform Winner
+        # TODO-8 Ask to play again
+        # TODO-9 Play Again or End game
+
+
     display_board(game_board.board)
-
-
