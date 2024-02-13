bf7d8e29fc016efa6079177910f38e1578046712
app-kaisha <app.kaihatsusha@gmail.com>
2024-02-13 14:07:52 +0000

    refactor to class

--- main.py
+++ main.py
@@ -5,74 +5,73 @@ class GameBoard:
             " ", " ", " ",
             " ", " ", " ",
         ]
+        self.player = 0
+        self.is_game = True
+        self.display_game_board()
 
+    def display_game_board(self):
+        print("\n" + self.board[0] + " | " + self.board[1] + " | " + self.board[2])
+        print("---------")
+        print(self.board[3] + " | " + self.board[4] + " | " + self.board[5])
+        print("---------")
+        print(self.board[6] + " | " + self.board[7] + " | " + self.board[8])
 
-def display_board(board):
-    print(board[0] + " | " + board[1] + " | " + board[2])
-    print("---------")
-    print(board[3] + " | " + board[4] + " | " + board[5])
-    print("---------")
-    print(board[6] + " | " + board[7] + " | " + board[8])
+    def change_player(self):
+        self.player = int(not self.player)
 
+    def get_player_symbol(self):
+        return 'X' if self.player == 0 else 'O'
 
-def make_player_move():
-    # TODO-2 Ask player to enter grid position for symbol
-    player_move = int(input("Position: "))
-    # TODO-3 Check player position is valid
-    if game_board.board[player_move] != " ":
-        print("Sorry that position is already taken! Select another!")
-        make_player_move()
-    else:
-        return player_move
+    def validate_move(self):
+        print(f'Player {self.player + 1} please select a grid position to place your {self.get_player_symbol()}')
+        player_move = int(input("Position: "))
+        try:
+            if self.board[player_move] != " ":
+                print("Sorry that position is already taken! Select another!")
+                self.validate_move()
+            else:
+                self.board[player_move] = self.get_player_symbol()
+        except IndexError:
+            print("Out of range of game board! Try again.")
+            self.validate_move()
 
+    def check_win(self):
+        row_win = (self.board[0] == self.board[1] == self.board[2] == self.get_player_symbol()) | \
+                  (self.board[3] == self.board[4] == self.board[5] == self.get_player_symbol()) | \
+                  (self.board[6] == self.board[7] == self.board[8] == self.get_player_symbol())
+        col_win = (self.board[0] == self.board[3] == self.board[6] == self.get_player_symbol()) | \
+                  (self.board[1] == self.board[4] == self.board[7] == self.get_player_symbol()) | \
+                  (self.board[2] == self.board[5] == self.board[8] == self.get_player_symbol())
+        diag_win = (self.board[0] == self.board[4] == self.board[8] == self.get_player_symbol()) | \
+                   (self.board[2] == self.board[4] == self.board[6] == self.get_player_symbol())
 
-def check_win(board, symbol):
-    row_win = (board[0] == board[1] == board[2] == symbol) | \
-              (board[3] == board[4] == board[5] == symbol) | \
-              (board[6] == board[7] == board[8] == symbol)
-    col_win = (board[0] == board[3] == board[6] == symbol) | \
-              (board[1] == board[4] == board[7] == symbol) | \
-              (board[2] == board[5] == board[8] == symbol)
-    diag_win = (board[0] == board[4] == board[8] == symbol) | \
-               (board[2] == board[4] == board[6] == symbol)
-
-    if row_win | col_win | diag_win:
-        print(f'winner: {symbol}')
-    else:
-        print("no win")
+        self.display_game_board()
+        if row_win | col_win | diag_win:
+            print(f'winner: {self.get_player_symbol()}\n')
+            self.is_game = False
+        else:
+            self.change_player()
+            print("no win\n")
 
 
 if __name__ == "__main__":
-    print("TicTacToe\n")
+    print("TicTacToe")
 
-    players = {
-        1: "X",
-        2: "O"
-    }
+    game = GameBoard()
 
-    game_board = GameBoard()
+    game.board[0] = 'X'
+    game.board[6] = 'O'
+    game.board[2] = 'O'
+    game.board[5] = 'X'
+    game.board[8] = 'X'
 
-    play_game = True
-    current_player = 1
+    while game.is_game:
 
-    game_board.board[0] = 'X'
-    game_board.board[6] = 'O'
-    game_board.board[2] = 'O'
-    game_board.board[5] = 'X'
-    game_board.board[8] = 'X'
+        game.validate_move()
+        # TODO-5 Check if win condition exists
+        game.check_win()
 
+        # TODO-7 Inform Winner
+        # TODO-8 Ask to play again
+        # TODO-9 Play Again or End game
 
-    # TODO-1 Inform Players to decide who is X to go first
-    print(f'Player {current_player} please select a grid position to place your {players[current_player]}')
-
-    valid_player_move = make_player_move()
-    # TODO-4 Update game board with position
-    game_board.board[valid_player_move] = players[current_player]
-    # TODO-5 Check if win condition exists
-    check_win(game_board.board, players[current_player])
-    # TODO-6 Change Player
-    # TODO-7 Inform Winner
-    # TODO-8 Ask to play again
-    # TODO-9 Play Again or End game
-
-    display_board(game_board.board)
