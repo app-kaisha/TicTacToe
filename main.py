d995567a2acb8e758b91d28d38aa87d69c79ee7e
app-kaisha <app.kaihatsusha@gmail.com>
2024-02-13 12:20:16 +0000

    TODO items listed

--- main.py
+++ main.py
@@ -22,6 +22,16 @@ if __name__ == "__main__":
 
     play_game = True
 
+    # TODO-1 Inform Players to decide who is X to go first
+    # TODO-2 Ask player to enter grid position for symbol
+    # TODO-3 Check player position is valid
+    # TODO-4 Update game board with position
+    # TODO-5 Check if win condition exists
+    # TODO-6 Change Player
+    # TODO-7 Inform Winner
+    # TODO-8 Ask to play again
+    # TODO-9 Play Again or End game
+
     game_board.board[0] = 'X'
     game_board.board[4] = 'X'
     game_board.board[8] = 'X'
